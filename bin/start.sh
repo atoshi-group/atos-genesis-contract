65d6eb5619c5b59521b001fe123881ad202c40f4
edy <edy@lxl.local>
2024-08-30 17:46:15 +0800

    auto add staticnodes

--- bin/start.sh
+++ bin/start.sh
@@ -33,15 +33,20 @@ rm -rf node/node*/{geth,logs}
 ./geth init --datadir node/node5  genesis.json
 }
 
+function replace(){
+
+	# 提取 enode 地址并组成数组
+	nodes=$(cat node/node*/logs/core.log | grep enode: | awk -F"=" '{print $5"="$6}' | uniq | paste -sd "," -)
+	echo $nodes
+	# 替换 config.toml 中的 bootnodes 数组,linux执行的时候去掉''，mac使用的时候才需要
+	sed -i ''  "s|^StaticNodes = \[.*\]|StaticNodes = [$nodes]|" node/node*/config.toml
+}
+
 function start_geth(){
 nohup node/node1/geth1 --networkid 1167 --config node/node1/config.toml --datadir node/node1 --unlock 0xa4eCD346d065827d303E95934eD712E978693d97 --password node/password.txt --mine --allow-insecure-unlock --cache 8000 --verbosity 5 --gcmode=full --nodiscover >logs/1node.log 2>&1 &
-sleep 2
 nohup node/node2/geth2 --networkid 1167 --config node/node2/config.toml --datadir node/node2 --unlock 0x2af1516cba4b8abd55e98ed2aabf91d367f02734 --password node/password.txt --mine --allow-insecure-unlock --cache 8000 --verbosity 5 --gcmode=full --nodiscover >logs/2node.log 2>&1 &
-sleep 2
 nohup node/node3/geth3 --networkid 1167 --config node/node3/config.toml --datadir node/node3 --unlock 0x217d71773caf8916484800b959248dafc44a0629 --password node/password.txt --mine --allow-insecure-unlock --cache 8000 --verbosity 5 --gcmode=full --nodiscover >logs/3node.log 2>&1 &
-sleep 2
 nohup node/node4/geth4 --networkid 1167 --config node/node4/config.toml --datadir node/node4 --unlock 0x82f74b5adc6cc4acac54d80a2559317284fe2b87 --password node/password.txt --mine --allow-insecure-unlock --cache 8000 --verbosity 5 --gcmode=full --nodiscover >logs/4node.log 2>&1 &
-sleep 10
 nohup node/node5/geth5 --networkid 1167 --config node/node5/config.toml --datadir node/node5 --unlock 0x68786fe80f10449c6cf3acd97299facf15050721 --password node/password.txt --mine --allow-insecure-unlock --cache 8000 --verbosity 5 --gcmode=full --nodiscover >logs/5node.log 2>&1 &
 }
 
@@ -62,6 +67,10 @@ reset)
 	sleep 2
     del_data
 	init
+	start_geth
+	sleep 5
+	replace
+	stop_geth
 	start_geth
     ;;
 stop)
